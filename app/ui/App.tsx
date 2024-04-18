65aa3bd24359f8e05870e792de38bc2e2068a7c3
NoamBechhofer <noam.bechhofer@gmail.com>
2024-04-17 20:36:05 -0400

    use db for notes

--- app/ui/App.tsx
+++ app/ui/App.tsx
@@ -1,24 +1,20 @@
-"use client";
+'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Note from './Note';
 import NewNote from './NewNote';
-import { createNote, getNotes, deleteNote } from '@/app/lib/placeholder-data';
+import { createNote, getNotes, deleteNote } from '@/app/lib/data';
+import { Note as NoteType } from '../lib/definitions';
 
 import { assert } from 'chai';
 
 export default function App() {
-  const notes = getNotes();
   let [notes_components_state, set_notes_components_state] = React.useState(
     new Array<React.JSX.Element>(),
   );
 
-  const render_note = (note: {
-    key: number;
-    title: string | null;
-    content: string | null;
-  }) => {
-    const key = String(note.key) + '-note';
+  const render_note = (note: NoteType) => {
+    const key = String(note.id) + '-note';
     set_notes_components_state((prevNotes) => {
       return [
         ...prevNotes,
@@ -26,48 +22,53 @@ export default function App() {
           key={key}
           title={note.title ? note.title : ''}
           content={note.content ? note.content : ''}
-          delete_func={() => delete_note(key)}
+          delete_func={() => remove_note(key)}
         ></Note>,
       ];
     });
   };
 
   function add_note(note: { title: string; content: string }) {
-    const db_note = createNote(note);
-    render_note({
-      key: db_note[0].key,
-      title: db_note[0].title ? db_note[0].title : '',
-      content: db_note[0].content ? db_note[0].content : '',
-    });
+    createNote(note)
+      .then((db_note) => {
+        assert(db_note.length === 1);
+        render_note({
+          id: db_note[0].id,
+          title: db_note[0].title ? db_note[0].title : '',
+          content: db_note[0].content ? db_note[0].content : '',
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  function delete_note(key: string) {
+  function remove_note(key: string) {
     const id = parseInt(key.split('-')[0]);
-    deleteNote(id);
-    set_notes_components_state((prevNotes) =>
-      prevNotes.filter((note) => note.key !== key),
-    );
+    deleteNote(id)
+      .then(() => {
+        set_notes_components_state((prevNotes) =>
+          prevNotes.filter((note) => note.key !== key),
+        );
+      })
+      .catch((err) => console.log(err));
   }
 
-  /*
-   * Only need to render the pre-loaded notes once - the empty array indicates
-   * this to React.
-   * This renders the preloaded notes when the page is first loaded. See
-   * `../resources/notes.ts`
-   */
   /**
-   * Flag to check if we've already rendered the preloaded notes. This is
-   * necessary because in development mode, the useEffect hook runs twice.
+   * In strict mode, components render twice. This boolean flag will prevent us
+   * from loading (and subsequently rendering) the notes from the database
+   * twice.
    */
-  let didInit = false;
-  React.useEffect(() => {
-    // Only run once
-    if (didInit) {
-      return;
-    } else {
-      didInit = true;
-    }
-    notes.forEach((note) => render_note(note));
+  let already_rendered = false;
+  
+  useEffect(() => {
+    if (already_rendered) return;
+    else already_rendered = true;
+    getNotes()
+      .then((notes) => {
+        notes.forEach((note) => render_note(note));
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
