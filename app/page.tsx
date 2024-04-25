5582e254e80ce855b08e2b06b4a9e4bb83bf1a74
NoamBechhofer <noam.bechhofer@gmail.com>
2024-04-25 11:37:09 -0400

    metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import App from "./ui/App";
 
+export const metadata = {
+    title: "Notes",
+    applicationName: "NKeeper",
+    authors: [{name:"Noam Bechhofer", url: "https://github.com/NoamBechhofer/"}],
+    creator: "Noam Bechhofer",
+    generator: "next.js",
+    keywords: ["notes", "note-taking", "app", "todo", "to-do"],
+    description: "A simple note-taking app",
+};
 export default function Page() {
     return <App />;
 }
