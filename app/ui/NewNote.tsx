a4fa778a67ccc404fa650675ffb1afed2c1f6f07
NoamBechhofer <noam.bechhofer@gmail.com>
2024-04-25 17:35:55 -0400

    formatting

--- app/ui/NewNote.tsx
+++ app/ui/NewNote.tsx
@@ -4,54 +4,55 @@
  * @details This component is used to create new notes.
  */
 
-import React from "react";
+import { useSession } from 'next-auth/react';
+import React from 'react';
 export default function NewNote(props: {
-	submit_func: (note: { title: string; content: string }) => void;
+  submit_func: (note: { title: string; content: string }) => void;
 }) {
-	let [note, setNote] = React.useState({ title: "", content: "" });
+  let [note, setNote] = React.useState({ title: '', content: '' });
 
-	const handleChange = (
-		event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-	) => {
-		const new_str: string = event.target.value;
-		const source: string = event.target.name;
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const new_str: string = event.target.value;
+    const source: string = event.target.name;
 
-		setNote((prev: { title: string; content: string }) => {
-			if (source === "title") {
-				return { title: new_str, content: prev.content };
-			} else if (source === "content") {
-				return { title: prev.title, content: new_str };
-			} else {
-				console.assert(false, "Invalid source");
-				return prev;
-			}
-		});
-	};
-	return (
-		<form>
-			<input
-				name="title"
-				placeholder="Title"
-				onChange={handleChange}
-				value={note.title}
-			/>
-			<textarea
-				name="content"
-				placeholder="Take a note..."
-				onChange={handleChange}
-				value={note.content}
-				rows={3}
-			/>
-			<button
-				type="submit"
-				onClick={(e) => {
-					e.preventDefault();
-					props.submit_func(note);
-					setNote({ title: "", content: "" });
-				}}
-			>
-				+
-			</button>
-		</form>
-	);
+    setNote((prev: { title: string; content: string }) => {
+      if (source === 'title') {
+        return { title: new_str, content: prev.content };
+      } else if (source === 'content') {
+        return { title: prev.title, content: new_str };
+      } else {
+        console.assert(false, 'Invalid source');
+        return prev;
+      }
+    });
+  };
+  return (
+    <form>
+      <input
+        name="title"
+        placeholder="Title"
+        onChange={handleChange}
+        value={note.title}
+      />
+      <textarea
+        name="content"
+        placeholder="Take a note..."
+        onChange={handleChange}
+        value={note.content}
+        rows={3}
+      />
+      <button
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          props.submit_func(note);
+          setNote({ title: '', content: '' });
+        }}
+      >
+        +
+      </button>
+    </form>
+  );
 }
